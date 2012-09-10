a6124a1e9aec968354f328fa202bde05bc7226cb
rshepherd <me@rshepherd.org>
2012-09-10 10:49:26 -0400

    Creating base mavenized java/scala project.

---
+++ src/main/java/hps/mint/App.java
@@ -0,0 +1,14 @@
+package hps.mint;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        new App().test();
+    }
+
+    public void test()
+    {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/hps/mint/AppTest.java
@@ -0,0 +1,14 @@
+package hps.mint;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class AppTest 
+{
+    @Test
+    public void testApp()
+    {
+        new App().test();
+        assertTrue(true);
+    }
+}
